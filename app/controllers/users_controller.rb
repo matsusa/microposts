dbbf414e067cdfec89ce790f6ea65d3734ccfe6c
matsusa <llmatsusall@gmail.com>
2018-11-30 17:43:40 +0000

    add likes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,56 @@
+class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+
+  def index
+    @users = User.all.page(params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.order('created_at DESC').page(params[:page])
+    counts(@user)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
+  end
+
+  def followings
+    @user = User.find(params[:id])
+    @users = @user.followings.page(params[:page])
+    counts(@user)
+    render :show_follow
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.followers.page(params[:page])
+    counts(@user)
+    render :show_follow
+  end
+
+  def likes
+    @user = User.find(params[:id])
+    p @microposts = @user.favorite_microposts.order('created_at DESC').page(params[:page])
+    counts(@user)
+    render :show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
---
+++ app/models/micropost.rb
@@ -0,0 +1,9 @@
+class Micropost < ApplicationRecord
+  belongs_to :user
+
+  validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 255 }
+
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
+end
